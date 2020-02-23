cc0dfe609569cacec8d49d68eacb93aef2ef6285
hosylibi <hosylibi@gmail.com>
2020-02-23 23:31:31 +0700

    #layout

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Routing\Controller as BaseController;
+
+class HomeController extends BaseController
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return view('home.home', [
+            'data' => [
+
+            ]
+        ]);
+    }
+}
